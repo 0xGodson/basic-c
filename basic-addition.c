917c2b4b508f9f2333d1fbd3af255aa5c5f43adf
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 21:23:52 +0530

    Create basic-addition.c

---
+++ basic-addition.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+        // you can change the values here
+        int a=17,b=18;
+        int total = 0;
+        total = a + b;
+        printf("%d + %d = %d \n",a,b,total);
+}
