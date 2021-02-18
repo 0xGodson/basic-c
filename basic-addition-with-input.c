41e7adea848399f6b76d477ed8e572a42f11d9fd
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-02-18 21:25:14 +0530

    Create basic-addition-with-input.c

---
+++ basic-addition-with-input.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+        // declaring variable here
+        int a,b;  
+        int total;
+
+        // to get input use scanf function
+        printf("Enter a: ");
+        scanf("%d",&a);
+        printf("Enter b: ");
+        scanf("%d",&b);
+
+        // performing addition
+        total = a + b;
+        printf("\n%d + %d = %d",a,b,total);
+ 
+}
