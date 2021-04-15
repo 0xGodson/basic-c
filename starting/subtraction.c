69279ccf6fe902924e9ee689411a89bcf59227ef
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-04-15 22:48:46 +0530

    Update subtraction.c

--- starting/subtraction.c
+++ starting/subtraction.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int main(){
-	int a,b,total;
+	int a,b,c,total;
 
 	printf("Enter a: ");
 	scanf("%d",&a);
 	printf("Enter b: ");
 	scanf("%d",&b);
+	printf("Enter c: ");
+        scanf("%d",&c);
 
-	total = a-b;
-	printf("\n%d - %d = %d\n",a,b,total);
+	total = a-b-c;
+	printf("\n%d - %d - %d  = %d\n",a,b,c,total);
 	return 0;
 }
