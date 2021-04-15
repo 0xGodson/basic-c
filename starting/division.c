f2eb70f396784e4e3bbcecc37dce92ca10427a58
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-04-15 22:25:18 +0530

    Create division.c

---
+++ starting/division.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int a,b,total;
+
+	printf("Enter a: ");
+	scanf("%d",&a);
+	printf("Enter b: ");
+	scanf("%d",&b);
+
+	total = a/b;
+	printf("\n%d / %d = %d\n",a,b,total);
+	return 0;
+}
