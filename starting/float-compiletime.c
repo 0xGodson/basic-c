6223bfdac2b46031d72e42375867b91102a63484
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-04-19 08:54:10 +0530

    Create float-compiletime.c

---
+++ starting/float-compiletime.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+        float a=2.5,b=1.8,c=18.17;
+
+        printf("\nFirst number: %f",a);
+        printf("\nSecond number: %f",b);
+        printf("\nThird number: %f",c);
+        return 0;
+}
