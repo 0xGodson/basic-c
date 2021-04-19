534faf08649b7d1e1057171b903bb891a5272d8d
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-04-19 08:48:33 +0530

    Create float-runtime.c

---
+++ starting/float-runtime.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){                                                        float a,b,c;
+        printf("Enter a:");
+        scanf("%f",&a);
+        printf("Enter b:");
+        scanf("%f",&b);
+        printf("Enter c:");
+        scanf("%f",&c);
+
+        printf("\nFirst number: %f",a);
+        printf("\nSecond number: %f",b);
+        printf("\nThird number: %f",c);
+        return 0;
+}
