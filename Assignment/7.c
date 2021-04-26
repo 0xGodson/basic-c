238d2be4fce6a616d6d0af7230acad450a371f01
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-04-27 00:52:50 +0530

    Create 7.c

---
+++ Assignment/7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+	
+	double w1, c1, w2, c2, result;
+
+  printf("Weight - Item1: ");
+	scanf("%lf", &w1);
+	printf("No. of item1: ");
+	scanf("%lf", &c1);
+	printf("Weight - Item2: ");
+	scanf("%lf", &w2);
+	printf("No. of item2: ");
+	scanf("%lf", &c2);
+
+	result = ((w1 * c1) + (w2 * c2)) / (c1 + c2);
+	printf("\nAverage Value = %f\n", result);
+}
