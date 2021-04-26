a75e60dc6a9aac4c3864c6d496c2bc5de6818528
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-04-27 00:51:56 +0530

    Create 6.c

---
+++ Assignment/6.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+void main(){
+
+   int radius = 6;
+   float area, perimeter;    
+
+   perimeter = 2*3.14*radius;
+   area = 3.14*radius*radius;
+
+   printf("Perimeter of the Circle = %f\n", perimeter);
+   printf("Area of the Circle = %f\n", area);
+}
