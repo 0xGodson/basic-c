497522ec687449aa3ce0970e9fb0da8bf49155c8
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-04-27 00:49:03 +0530

    Create 3.c

---
+++ Assignment/3.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+ void main(){
+
+	printf("######\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#####\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+
+	printf("\n    ######\n");
+  printf("  ##      ##\n");
+  printf(" #\n");
+  printf(" #\n");
+  printf(" #\n");
+  printf(" #\n");
+  printf(" #\n");
+  printf("  ##      ##\n");
+  printf("    ######\n");
+}
