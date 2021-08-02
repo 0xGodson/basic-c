7c8f41cf74a69374290c18ccbe6afdb9a392158f
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-08-02 22:14:17 +0530

    Create 26-a.c

---
+++ CSE-PRACTICALS-FORENOON/26-a.c
@@ -0,0 +1,15 @@
+// Write and execute a C program to find whether the given number is even or odd
+
+// program
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+    
+    return 0;
+}
