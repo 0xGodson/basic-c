f72c0cd49e30911d0ec0ce905b80391efa055f82
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-08-03 01:50:30 +0530

    Create 21-a.c

---
+++ CSE-PRACTICALS-FORENOON/21-a.c
@@ -0,0 +1,19 @@
+/*
+Write and execute a C program to find maximum of given three numbers using parameter Passing
+*/
+
+// program:
+#include<stdio.h>
+float large(float a, float b, float c){
+   if(a>=b && a>=c) return a;
+   else if(b>=a && b>=c) return b;
+   else return c;
+}
+int main(){
+   float num1, num2, num3, largest;
+   printf("Enter three numbers: ");
+   scanf("%f %f %f", &num1, &num2, &num3);
+   largest = large(num1, num2, num3);
+   printf("Largest number = %.2f",largest);
+   return 0;
+}
