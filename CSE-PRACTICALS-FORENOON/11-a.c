73c64533bd3170bac3eadef9db71da439527d9b2
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-08-02 22:33:08 +0530

    Create 11-a.c

---
+++ CSE-PRACTICALS-FORENOON/11-a.c
@@ -0,0 +1,24 @@
+// Write and execute a C Program to find whether a given number is a prime number
+
+// program
+#include <stdio.h>
+void main() {
+  int n, i, flag = 0;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+  for (i = 2; i <= n / 2; ++i) {
+    if (n % i == 0) {
+      flag = 1;
+      break;
+    }
+  }
+  if (n == 1) {
+    printf("1 is neither prime nor composite.");
+  } 
+  else {
+    if (flag == 0)
+      printf("%d is a prime number.", n);
+    else
+      printf("%d is not a prime number.", n);
+  }
+}
