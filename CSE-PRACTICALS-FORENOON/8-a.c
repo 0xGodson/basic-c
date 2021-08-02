89d84d1794ba515a989c299f5d7499fecb0469f4
Jopraveen <56404692+jopraveen@users.noreply.github.com>
2021-08-02 23:26:32 +0530

    Create 8-a.c

---
+++ CSE-PRACTICALS-FORENOON/8-a.c
@@ -0,0 +1,26 @@
+// Write and execute a C program for sorting an array of N data in Ascending order
+
+// program
+#include <stdio.h>
+void main (){
+   int num[20];
+   int i, j, a, n;
+   printf("enter number of elements in an array: ");
+   scanf("%d", &n);
+   printf("Enter the elements:\n");
+   for (i = 0; i < n; ++i)
+      scanf("%d", &num[i]);
+   for (i = 0; i < n; ++i){
+      for (j = i + 1; j < n; ++j){
+         if (num[i] > num[j]){
+            a = num[i];
+            num[i] = num[j];
+            num[j] = a;
+         }
+      }
+   }
+   printf("The numbers in ascending order is:\n");
+   for (i = 0; i < n; ++i){
+      printf("%d\n", num[i]);
+   }
+}
